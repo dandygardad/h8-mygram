0de157640e61116934f0c81a9a51ee79c9f62c48
dandygardad <dandygardad@gmail.com>
2023-04-19 14:28:22 +0700

    forgot swag init

--- docs/docs.go
+++ docs/docs.go
@@ -1023,7 +1023,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "https://h8-mygram-production.up.railway.app",
+	Host:             "h8-mygram-production.up.railway.app",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "MyGram API",
